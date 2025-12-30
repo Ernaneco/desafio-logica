d882cf2bb3266f6c8b742913e8350c876731a8df
Ernaneco <104723811+Ernaneco@users.noreply.github.com>
2025-12-30 10:48:19 -0300

    Desafio de classificador de herói concluído.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Desafio");
+let nomeHeroi = "Escudo Verde";
+let experiencia = 8000;
+let nivel = "";
+
+if (experiencia < 1000) nivel = "Ferro";
+else if (experiencia < 2000) nivel = "Bronze";
+else if (experiencia < 5000) nivel = "Prata";
+else if (experiencia < 7000) nivel = "Ouro";
+else if (experiencia < 8000) nivel = "Platina";
+else if (experiencia < 9000) nivel = "Ascendente";
+else if (experiencia < 10000) nivel = "Imortal";
+else nivel = "Radiante";
+
+console.log(`O Héroi de nome ${nomeHeroi} está no nível de ${nivel}.`);
